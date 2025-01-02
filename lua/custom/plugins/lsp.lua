d92da213b90e2934d4e40a05b723bd535cae8d03
Maxwell Knight <maxwell.knight98@gmail.com>
2025-01-02 15:38:08 +0200

    added missing parameter in lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -43,7 +43,7 @@ return {
         -- Inlay hints
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
           end, '[T]oggle Inlay [H]ints')
         end
       end,
