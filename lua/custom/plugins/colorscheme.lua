5fbae2c4f5c41c60d6f2ad3d30ea0de47650036e
Maxwell Knight <maxwell.knight98@gmail.com>
2025-02-04 15:10:25 +0200

    fixed telescope background by forcing inside colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
 return {
-  'folke/tokyonight.nvim',
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000,
   init = function()
-    -- Load the colorscheme
+    require('catppuccin').setup {
+      transparent_background = true,
+    }
+
     vim.cmd.colorscheme 'catppuccin'
-    -- Configure highlights
     vim.cmd.hi 'Comment gui=none'
+
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = function()
+        vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg = 'none' })
+      end,
+    })
   end,
 }
